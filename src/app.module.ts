b61b4cde2d700aa6c775e837f3595821c75f9070
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-10-03 22:12:08 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+//this is a decorator; is a function that  adds a metadata to the current class or function that is decorating
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
