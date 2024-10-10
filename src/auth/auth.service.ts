1e7972eec9caf5590f7a92b38488ba0ec3a8081e
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-10-10 21:43:05 +0100

    signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
@@ -14,36 +18,71 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-   
     try {
-        //generate the password
-        const hash = await argon.hash(dto.password)
+      //generate the password
+      const hash = await argon.hash(dto.password);
 
-        //save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash
-            }
-        });
+      //save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-        delete user.hash;
+      delete user.hash;
 
-        //return the saved user
-        return user;
-
+      //return the saved user
+      return user;
     } catch (error) {
-        if(error instanceof PrismaClientKnownRequestError){
-            if(error.code === "P2002"){
-                throw new ForbiddenException('Credentials taken')
-            }
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
         }
+      }
     }
   }
 
-  signin() {
-    return {
-      msg: `I am signed in ${process.env.DATABASE_URL}`,
-    };
+  async signin(dto: AuthDto) {
+    //find user by email
+    const user = await this.prisma.user.findFirst(
+      {
+        where: {
+          email: dto.email,
+        },
+      },
+    );
+    console.log('user', user);
+
+    //if user does not exist => throw exceptiopn
+    if (!user) {
+      throw new NotFoundException(
+        'User not found',
+      );
+    }
+
+    //compare passwords
+    const isCorrectPassword = await argon.verify(
+        user.hash,
+        dto.password
+    );
+    //if password incorrect throw exception
+    if (!isCorrectPassword) {
+      throw new ForbiddenException(
+        'Wrong credentials',
+      );
+    }
+
+    //send back the user
+    delete user.hash;
+
+    return user;
   }
 }
+
+
