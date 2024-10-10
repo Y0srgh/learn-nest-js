3e0c5e2e8782840073b306b0514c6389aa878cca
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-10-10 20:20:20 +0100

    trying pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,31 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 //the auth is the prefixe route
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
-        //which literally means : i dont care how u'd instanciate it just
-        //give me an instance from that auth service, 
+  constructor(private authService: AuthService) {}
+  //which literally means : i dont care how u'd instanciate it just
+  //give me an instance from that auth service,
 
-        @Post('signup')
-        signup(@Body() dto: AuthDto){
-            console.log(dto);
-            
-            return this.authService.signup()
-        }
+  @Post('signup')
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
+    console.log({ 
+        email, 
+        typeOfEmail: typeof email,
+        password, 
+        typeOfPassword: typeof password 
+    });
 
-        @Post('signin')
-        signin(){
-            return this.authService.signin()
-        }
-    
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
